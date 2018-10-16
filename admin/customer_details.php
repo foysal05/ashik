34bcb7c9235dd957999e07daaf9fda3026b380ab
foysal <foysal35@diit.info>
2018-10-17 00:16:22 +0600

    C

--- admin/customer_details.php
+++ admin/customer_details.php
@@ -2,116 +2,151 @@
 <?php 
 session_start();
 if (isset($_SESSION['admin_login'])==TRUE) {
- 
-?>
-<!doctype html>
-<html lang="en">
-<?php 
-include('inc/head.php');
-include('../db/db.php');
 
-?>
-<body>
+    ?>
+    <!doctype html>
+    <html lang="en">
+    <?php 
+    include('inc/head.php');
+    include('../db/db.php');
 
-    <div class="wrapper">
-        <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
+    ?>
+    <body>
 
-            <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+        <div class="wrapper">
+            <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
 
-
+                <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
 
 
-            <?php include('inc/side_bar.php');?>
-
-            <div class="main-panel">
-              <?php include('inc/top_bar.php');?>
 
 
-              <?php
+                <?php include('inc/side_bar.php');?>
 
-              $query="SELECT * FROM customer where customer.cid='".$_GET['id']."'";
-              $result=mysqli_query($con,$query);
+                <div class="main-panel">
+                  <?php include('inc/top_bar.php');?>
+
+
+                  <?php
+
+                  $query="SELECT * FROM customer where customer.cid='".$_GET['id']."'";
+                  $result=mysqli_query($con,$query);
                 //echo mysqli_error();
-              if(mysqli_num_rows($result)>0){
+                  if(mysqli_num_rows($result)>0){
 
-                  while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                      while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
 
-                    ?>
-                    <div class="content">
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-8">
-                                    <div class="card">
-                                        <div class="header">
-                                            <h4 class="title">Edit Profile</h4>
-                                        </div>
-                                        <div class="content">
-                                            <form>
-                                               
+                        ?>
+                        <div class="content">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <div class="card">
+                                            <div class="header">
+                                                <h4 class="title">Edit Profile</h4>
+                                            </div>
+                                            <div class="content">
+                                                <form>
 
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label>First Name</label>
-                                                            <input type="text" class="form-control" placeholder="Company" name="firstname" value="<?php echo $row['firstname'];?>">
+
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label>First Name</label>
+                                                                <input type="text" class="form-control" placeholder="Company" name="firstname" value="<?php echo $row['firstname'];?>">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label>Last Name</label>
+                                                                <input type="text" class="form-control" placeholder="Last Name" name="lastname" value="<?php echo $row['lastname'];?>">
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label>Last Name</label>
-                                                            <input type="text" class="form-control" placeholder="Last Name" name="lastname" value="<?php echo $row['lastname'];?>">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                 <div class="row">
-                                                  
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="exampleInputEmail1">Email address</label>
-                                                            <input type="email" name="email" value="<?php echo $row['email'];?>" class="form-control" placeholder="Email">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="exampleInputEmail1">Phone</label>
-                                                            <input type="text" name="phone" value="<?php echo $row['phone'];?>" class="form-control" placeholder="Phone">
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                                    <div class="row">
 
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="form-group">
-                                                            <label>Address</label>
-                                                            <input type="text" class="form-control" placeholder="Home Address" name="address" value="<?php echo $row['address'];?>">
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label for="exampleInputEmail1">Email address</label>
+                                                                <input type="email" name="email" value="<?php echo $row['email'];?>" class="form-control" placeholder="Email">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <label for="exampleInputEmail1">Phone</label>
+                                                                <input type="text" name="phone" value="<?php echo $row['phone'];?>" class="form-control" placeholder="Phone">
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
 
-                                                
-                                               
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                <label>Address</label>
+                                                                <input type="text" class="form-control" placeholder="Home Address" name="address" value="<?php echo $row['address'];?>">
+                                                            </div>
+                                                        </div>
+                                                    </div>
 
-                                                <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
-                                                <div class="clearfix"></div>
-                                            </form>
+
+
+
+                                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
+                                                    <div class="clearfix"></div>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="card card-user">
-                                        <div class="image">
-                                            <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
-                                        </div>
-                                        <div class="content">
-                                            <div class="author">
-                                               <a href="#">
-                                                <img class="avatar border-gray" src="assets/img/faces/face-3.jpg" alt="..."/>
+                                    <div class="col-md-4">
+                                        <div class="card card-user">
+                                            <div class="image">
+                                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+                                            </div>
 
-                                                <h4 class="title"><?php echo $row['firstname']." ".$row['lastname'];?><br />
-                                                   <small><?php echo $row['email'];?></small>
-                                               </h4>
-                                           </a>
-                                       </div>
+                                            <div class="content" style="margin-top: 10px;">
+                                                <div class="author">
+                                                    <br>
+                                                    <br>
+                                                    <br>
+                                                    <br>
+                                                    <br>
+
+
+                                                    <table class="table table-hover table-striped">
+                                                        <head style="font-size: 2em">Booking History</head>
+                                                        <tr>
+                                                         <th style="text-align: center;">Date</th>
+                                                         <th style="text-align: center;">Movie</th>
+                                                     </tr>
+                                                  
+                                                     <?php
+
+$query1="SELECT date,movie_name FROM booking,movie where booking.movie=movie.mid and cid='".$_GET['id']."'";
+            // echo $query1;
+             $result1=mysqli_query($con,$query1);
+//echo mysqli_error();
+             if(mysqli_num_rows($result1)>0){
+
+              while($booking=mysqli_fetch_array($result1, MYSQLI_ASSOC)){
+                echo "<tr>";
+                 echo "<td>".$booking['date']."</td>";
+                echo "<td>".$booking['movie_name']."</td>";
+                echo "</tr>";
+
+
+
+              }
+          }else{
+            echo "<tr>";
+                 echo "<td colspan='2'>Booking Not Found !</td>";
+               
+                echo "</tr>";
+          }
+
+                 ?>
+                                                     </table>
+
+                                                 </div>
                                        <!-- <p class="description text-center"> "Lamborghini Mercy <br>
                                         Your chick she so thirsty <br>
                                         I'm in that two seat Lambo"
@@ -131,14 +166,14 @@ include('../db/db.php');
                 </div>
             </div>
 
-<?php
-}
-}
-?>
-             <?php include('inc/footer.php');?>
+            <?php
+        }
+    }
+    ?>
+    <?php include('inc/footer.php');?>
 
 
-    </div>
+</div>
 </div>
 
 
